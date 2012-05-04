d0ac15b4aea0728a93bb522ee90e6f90618b9594
Bradley Spaulding <bspaulding@dataxu.com>
2012-05-04 12:13:27 -0400

    Added initial rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jerisa::Application.config.secret_token = '051543f34be8215881f73326ab85cc2729a9b3224e3a5d690a8cf3325c920700760012ab51c1aac85d643f5488c29f257b9c5eaefd6cda5d2b8ba3962304cca8'
