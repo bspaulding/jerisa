9228ac8f34c3cb777c0baba1c4f365ada312ebea
Bradley Spaulding <bspaulding@dataxu.com>
2012-06-11 10:31:05 -0400

    Updated layout, added static pages.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+  def our_story; end
+  def registry; end
+  def contact_us; end
+  def guestbook; end
+  def ceremony; end
+  def reception; end
+  def accomodations; end
+end
